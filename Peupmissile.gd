b047bcf6a1e1b6bf6931d1c0d01725b4fddfdfdf
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-11-23 00:03:34 -0800

    move more projectile code from specific projectiles into base class

--- Peupmissile.gd
+++ Peupmissile.gd
@@ -1,29 +1,5 @@
 extends "res://Projectile.gd"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _init():
 	speed = 300
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame
-func _process(delta):
-	# It seems that the parent class's _process automatically gets called even due to being "call_on_multilevel"
-	pass
-
-func explode():
-	detonated = true
-	$MissileSprite.visible=false
-	$MissileHitbox.disabled = true
-	$ExplosionSprite.visible=true
-	$ExplosionArea/ExplosionHitbox.disabled = false
-	$ExplosionAnimation.play("explosion")
-
-
-func _on_ExplosionAnimation_animation_finished(anim_name):
-	queue_free()
-
-
-
-func _on_ExplosionArea_body_shape_entered(body_id, body, body_shape, area_shape):
-	if $ExplosionSprite.frame < 6:
-		emit_signal("body_harmed",body)
+	explosion_max_deadly_frame = 5
--- Projectile.gd
+++ Projectile.gd
@@ -3,32 +3,40 @@ extends KinematicBody2D
 var velocity_modifier : Vector2
 var detonated = false
 var speed : int;
+var explosion_max_deadly_frame = INF
 
 signal body_harmed
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ExplosionAnimation.connect("animation_finished", self, "_on_ExplosionAnimation_animation_finished")
+	$ExplosionArea.connect("body_shape_entered",self,"_on_ExplosionArea_body_shape_entered")
+
 
 func set_trajectory(pos,rot,velocity_modifier=Vector2(0,0)) :
 	self.velocity_modifier = velocity_modifier
 	position = pos
 	rotation_degrees = rot
 
-# Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
 	if not detonated:
 		if move_and_collide((Vector2(0,-speed).rotated(deg2rad(rotation_degrees)) + velocity_modifier)*delta):
 			explode()
 
-func explode():
-	print("Explosion not implemented for ",self)
 
-#func explode():
-#	detonated = true
-#	$MissileSprite.visible=false
-#	$MissileHitbox.disabled = true
-#	$ExplosionSprite.visible=true
-#	$ExplosionArea/ExplosionHitbox.disabled = false
-#	$ExplosionAnimation.play("explosion")
+func explode():
+	detonated = true
+	$MissileSprite.visible=false
+	$MissileHitbox.disabled = true
+	$ExplosionSprite.visible=true
+	$ExplosionArea/ExplosionHitbox.disabled = false
+	$ExplosionAnimation.play("explosion")
+	
+	
+func _on_ExplosionAnimation_animation_finished(anim_name):
+	queue_free()
+
+
+func _on_ExplosionArea_body_shape_entered(body_id, body, body_shape, area_shape):
+	if $ExplosionSprite.frame <= explosion_max_deadly_frame:
+		emit_signal("body_harmed",body)
