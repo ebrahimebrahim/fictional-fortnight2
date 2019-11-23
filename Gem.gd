f81328245a85406dba76142c3c30bfa0e4b432d5
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-11-22 23:41:16 -0800

    inherit gem from monster.gd

--- Gem.gd
+++ Gem.gd
@@ -1,49 +1,15 @@
-extends KinematicBody2D
+extends "res://Monster.gd"
 
 
-var dead = false
 
-# Firing pattern is a list of pairs each consisting of a direction and wait time to follow it
-const fire_pattern = [
-	[0,1],
-	[90,1],
-	[180,1],
-	[270,1]
+func _init():
+	# Firing pattern is a list of pairs each consisting of a direction and wait time to follow it
+	fire_pattern = [
+		[0,1],
+		[90,1],
+		[180,1],
+		[270,1]
 	]
-
-# How far from monster position to spawn projectiles.
-var launch_dist = 50
-
-#index of fire pattern we are currently on
-var current_fire_stage = 0
-
-signal fire
-
-
-func _ready():
-	pass
-
-
-func _process(delta):
-	pass
 	
-
-func fire():
-	var launch_rot = fire_pattern[current_fire_stage][0]
-	var launch_pos = position + Vector2(0,-launch_dist).rotated(deg2rad(launch_rot))
-	emit_signal("fire",launch_pos,launch_rot)
-	$FireTimer.start(fire_pattern[current_fire_stage][1])
-	current_fire_stage = (current_fire_stage + 1) % fire_pattern.size()
-
-func die():
-	dead = true
-	$Hitbox.disabled = true
-	$IdleAnimation.stop()
-	$ExplodeAnimation.play("explode")
-
-func _on_ExplodeAnimation_animation_finished(anim_name):
-	queue_free()
-
-
-func _on_FireTimer_timeout():
-	fire()
+	# How far from monster position to spawn projectiles.
+	launch_dist = 50
---
+++ Monster.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+
+var dead = false
+
+# Firing pattern is a list of pairs each consisting of a direction and wait time to follow it
+# This should set for each monster
+var fire_pattern = []
+
+# How far from monster position to spawn projectiles.
+var launch_dist = 50
+
+#index of fire pattern we are currently on
+var current_fire_stage = 0
+
+signal fire
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+	
+
+func fire():
+	var launch_rot = fire_pattern[current_fire_stage][0]
+	var launch_pos = position + Vector2(0,-launch_dist).rotated(deg2rad(launch_rot))
+	emit_signal("fire",launch_pos,launch_rot)
+	$FireTimer.start(fire_pattern[current_fire_stage][1])
+	current_fire_stage = (current_fire_stage + 1) % fire_pattern.size()
+
+func die():
+	dead = true
+	$Hitbox.disabled = true
+	$IdleAnimation.stop()
+	$ExplodeAnimation.play("explode")
+
+func _on_ExplodeAnimation_animation_finished(anim_name):
+	queue_free()
+
+
+func _on_FireTimer_timeout():
+	fire()
