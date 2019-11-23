e6d1efebfdb9b9cf0d8884e75f43acd45a25a42d
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-11-23 12:04:18 -0800

    improve player orientation control

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var speed = 400
+
 const launch_point_dist = 50; # how far in front of player to spawn peupmissile
 
 var hp = 3;
@@ -14,6 +15,18 @@ signal dead
 
 enum {UP,DOWN,LEFT,RIGHT}
 export var orientation = UP
+var recent_orientation_presses = [UP,DOWN,LEFT,RIGHT]
+const orientation_to_ui = {UP:"ui_up",DOWN:"ui_down",LEFT:"ui_left",RIGHT:"ui_right"}
+var ui_to_orientation : Dictionary
+
+func _init():
+	for orientation in orientation_to_ui.keys():
+		ui_to_orientation[orientation_to_ui[orientation]] = orientation
+
+
+func update_recent_orientation_presses(new_orientation):
+	recent_orientation_presses.push_front(new_orientation)
+	recent_orientation_presses.pop_back()
 
 func is_dead() -> bool:
 	return hp<=0
@@ -72,6 +85,8 @@ func die():
 
 
 func _process(delta):
+	
+	# Set velocity vector
 	var velocity = Vector2()
 	if (Input.is_action_pressed("ui_right")):
 		velocity.x=1
@@ -81,17 +96,17 @@ func _process(delta):
 		velocity.y=-1
 	if (Input.is_action_pressed("ui_down")):
 		velocity.y=1
-		
-	if (Input.is_action_just_pressed("ui_right")):
-		orientation=RIGHT
-	if (Input.is_action_just_pressed("ui_left")):
-		orientation=LEFT
-	if (Input.is_action_just_pressed("ui_up")):
-		orientation=UP
-	if (Input.is_action_just_pressed("ui_down")):
-		orientation=DOWN
-		
 	velocity = velocity.normalized()*speed
+		
+	# Set orientation
+	for ui in ui_to_orientation.keys():
+		if (Input.is_action_just_pressed(ui)):
+			update_recent_orientation_presses(ui_to_orientation[ui])
+	for recent_orientation_press in recent_orientation_presses:
+		if Input.is_action_pressed(orientation_to_ui[recent_orientation_press]):
+			orientation = recent_orientation_press
+			break
+		
 	
 	if not is_dead():
 		move_and_collide(velocity*delta)
