e37ea3fe79bfd42a8b59f81931c0d58e03a37420
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-11-23 12:43:22 -0800

    add Octo

--- MonsterManager.gd
+++ MonsterManager.gd
@@ -2,27 +2,37 @@ extends Node
 
 const Gem = preload("res://Gem.tscn")
 const Shard = preload("res://Shard.tscn")
+
+const Octo = preload("res://Octo.tscn")
+const Ink = preload("res://Ink.tscn")
+
+const monster_to_projectile = {Gem:Shard, Octo:Ink}
+var monster_list = monster_to_projectile.keys()
+
 signal fire
 
 
 func _process(delta):
 	pass
 	
-func _on_gem_fire(launch_pos,launch_rot):
-	emit_signal("fire",Shard,launch_pos,launch_rot)
+func _on_monster_fire(launch_pos,launch_rot,projectile_scene):
+	emit_signal("fire",projectile_scene,launch_pos,launch_rot)
 
 func _on_ProjectileManager_body_harmed(body):
 	for monster in get_children():
 		if body == monster: monster.die()
 
-func _on_SpawnTimer_timeout():
-	var gem = Gem.instance()
-	gem.connect("fire",self,"_on_gem_fire")
-	add_child(gem)
+func spawn(monster_scene):
+	var monster = monster_scene.instance()
+	monster.connect("fire",self,"_on_monster_fire",[monster_to_projectile[monster_scene]])
+	add_child(monster)
 	
 	var viewport_texture_size = get_viewport().get_texture().get_size().floor()
 	var pos : Vector2
 	while true :
 		pos = Vector2(randi()%int(viewport_texture_size.x),randi()%int(viewport_texture_size.y))
-		if not gem.test_move(Transform2D(0,pos),Vector2()) : break
-	gem.position=pos
+		if not monster.test_move(Transform2D(0,pos),Vector2()) : break
+	monster.position=pos
+
+func _on_SpawnTimer_timeout():
+	spawn(monster_list[randi() % monster_list.size()])
---
+++ Octo.gd
@@ -0,0 +1,15 @@
+extends "res://Monster.gd"
+
+
+
+func _init():
+	# Firing pattern is a list of pairs each consisting of a direction and wait time to follow it
+	fire_pattern = [
+		[0,0.1],
+		[180,1],
+		[90,0.1],
+		[270,1]
+	]
+	
+	# How far from monster position to spawn projectiles.
+	launch_dist = 90
